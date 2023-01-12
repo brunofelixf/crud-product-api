7e1152d7b0e844ecc6e8d7d1b5f93816438b075b
brunofelixf <brunofelixf@gmail.com>
2023-01-12 13:47:54 -0300

    feat: add product schema

---
+++ prisma/migrations/20230112164219_product/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "category" VARCHAR(30) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "quantity" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
